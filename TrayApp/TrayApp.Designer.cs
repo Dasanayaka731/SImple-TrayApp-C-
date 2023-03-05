9919d7ea29a951896c61271e98bf16cb48d77cad
Dasun Dasanayaka <dasanayakabandara81@gmail.com>
2023-03-05 12:16:43 +0530

    Upload files

---
+++ TrayApp/TrayApp.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TrayApp
+{
+    partial class TrayApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.processlist = new System.Windows.Forms.ListBox();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.processlist);
+            this.groupBox1.Location = new System.Drawing.Point(1, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 349);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AllProcess";
+            // 
+            // processlist
+            // 
+            this.processlist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.processlist.FormattingEnabled = true;
+            this.processlist.Location = new System.Drawing.Point(3, 16);
+            this.processlist.Name = "processlist";
+            this.processlist.Size = new System.Drawing.Size(305, 330);
+            this.processlist.TabIndex = 0;
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(219, 357);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(80, 39);
+            this.btnKill.TabIndex = 1;
+            this.btnKill.Text = "Kill Process";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(112, 357);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(74, 39);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 357);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(66, 39);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add All";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // TrayApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 414);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TrayApp";
+            this.Text = "TrayApp";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox processlist;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ TrayApp/TrayApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayApp
+{
+    public partial class TrayApp : Form
+    {
+        public TrayApp()
+        {
+            InitializeComponent();
+        }
+        List<int> prolist = new List<int>();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            searchProcess();
+        }
+
+        private void searchProcess()
+        {
+            prolist = new List<int>();
+            processlist.Items.Clear();
+            Process[] p = Process.GetProcesses();
+            foreach (Process item in p)
+            {
+                processlist.Items.Add("Name: " + item.ProcessName + "   ID: " + item.Id);
+                prolist.Add(item.Id);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            searchProcess();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            if (processlist.SelectedIndices.Count > 0)
+            {
+                int id = prolist[processlist.SelectedIndex];
+                Process.GetProcessById(id).Kill();
+                searchProcess();
+            }
+        }
+    }
+}
